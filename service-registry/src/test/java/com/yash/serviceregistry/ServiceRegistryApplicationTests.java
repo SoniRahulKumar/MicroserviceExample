892f227903d47273c95bdae17f18df9bb57755ef
soni.rahul <soni.rahul@YI1012001LT.Yash.com>
2024-01-08 12:57:43 +0530

    Initial commit

---
+++ service-registry/src/test/java/com/yash/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
